f2bcc53a4fd7a4c4df19e897c9e850618c681816
Chris <chris.lovering.95@gmail.com>
2020-10-31 13:01:56 +0000

    remove looping

--- basics/logical_operators.cpp
+++ basics/logical_operators.cpp
@@ -3,21 +3,16 @@
 int main() {
 
     int year;
-    while(true){
-        std::cout << "Enter a 4 digit year: ";
-        std::cin >> year;
-        while(std::cin.fail()) {
-            std::cout << "That's not a number!" << std::endl;
-            std::cin.clear();
-            std::cin.ignore(256,'\n');
-            std::cout << "Enter a 4 digit year: ";
-            std::cin >> year;
-        }
-        if (std::to_string(year).length() != 4){
-            std::cout << "That's not a 4 digit number!" << std::endl;
-            continue;
-        }
-        break;
+    std::cout << "Enter a 4 digit year: ";
+    std::cin >> year;
+    if(std::cin.fail()) {
+        std::cout << "That's not a number!" << std::endl;
+        return 0;
+    }
+    if (std::to_string(year).length() != 4){
+        std::cout << "That's not a 4 digit number!" << std::endl;
+        return 0;
+    }
     }
     if (year % 4 == 0){
         if(year % 100 == 0 and !(year % 400 == 0)){
